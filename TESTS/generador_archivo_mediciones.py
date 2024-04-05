7ccfbde8b4065b675d78c19711c031b0c3371317
ColinaAndres <andrescolina725@gmail.com>
2024-04-05 16:40:55 -0300

    Grafico Actualizado,corregir ortografia

--- TESTS/generador_archivo_mediciones.py
+++ TESTS/generador_archivo_mediciones.py
@@ -7,11 +7,11 @@ def generar_mediciones():
         for i in range(1000,100001,1000):
             arr=leer_archivo(str(i)+".txt")
             suma_de_tiempos=0
-            for j in range(10):
+            for j in range(5):
                 inicio=time()
                 _ , _ = cal_sumatoria(arr)
                 fin=time()
                 suma_de_tiempos+=(fin-inicio)*1000
-            file.write(str(i)+","+str(suma_de_tiempos/10)+"\n")
+            file.write(str(i)+","+str(suma_de_tiempos/5)+"\n")
 
 generar_mediciones()
--- grafica.py
+++ grafica.py
@@ -9,17 +9,17 @@ with open("TESTS/Mediciones.txt","r") as file:
     lector=csv.reader(file,delimiter=",")
     file.readline()
     for linea in lector:
-        x.append(linea[0])
-        y.append(linea[1])
-    
+        x.append(int(linea[0]))
+        y.append(float(linea[1]))
 
-plt.plot(x,y,color='blue')
 
-# plt.plot(x,y1, color="green")
-
-plt.title('ALgoritmo de minima suma ponderada Greedy')
+figure,ax = plt.subplots()
+ax.plot(x,y,color="Red",label="cal_sumatoria")
+plt.title('Algoritmo de minima suma ponderada Greedy')
 plt.xlabel('Cantidad de elementos en Arreglo')
 plt.ylabel('Tiempo [ms]')
+plt.grid(True)
+ax.legend(loc='upper left')
 plt.show()
 
 
